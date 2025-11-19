ca8bed9acd999194f9d6dedbb8787711150d72f0
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2025-11-19 14:18:32 +0100

    Add function slugify

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function slugify(name) {
+    return String(name)
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^a-z0-9\-]/g, '');
+}
+
 fetch('data/playlist.json')
     .then((res) => res.json())
     .then((data) => {
