5f7a22db1bc3d335776fcc30709d308c75d5a674
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2025-11-19 14:22:48 +0100

    Add function createSection()

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ function slugify(name) {
         .replace(/[^a-z0-9\-]/g, '');
 }
 
+function createSection(name) {
+    const targetId = (slugify(name));
+
+    const title = document.createElement('h2');
+    title.id = targetId + '-h2';
+    title.innerText = name;
+
+    const container = document.createElement('section');
+    container.id = targetId;
+
+    document.body.appendChild(title);
+    document.body.appendChild(container);
+
+    return container;
+}
+
 fetch('data/playlist.json')
     .then((res) => res.json())
     .then((data) => {
-        
+        Object.entries(data).forEach(([playlistName, songs]) => {
+
+            const container = createSection(playlistName)
+        })
     });
